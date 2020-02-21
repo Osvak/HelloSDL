31ec7c50b1c839de888aacc3ddf0f66cf2dd07f4
Osvak <oscarcanales2001@gmail.com>
2020-02-21 09:56:21 +0100

    02/21
    First class with SDL

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "SDL/include/SDL.h"
+#pragma comment( lib, "SDL/libx86/SDL2.lib" )
+#pragma comment( lib, "SDL/libx86/SDL2main.lib" )
+
+int main(int argc, char** argv ) {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	
+
+	
+	return 0;
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# HelloSDL
- First project with SDL
